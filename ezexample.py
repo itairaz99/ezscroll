e324375c088e9e20bac436f1fd64c2f6965402d6
hrfeels <hrfeels@c7c4cef6-3a20-11de-be05-5f7a86268029>
2009-05-06 21:12:12 +0000

    Comments and handle no image

--- ezexample.py
+++ ezexample.py
@@ -1,5 +1,7 @@
 #! %PYTHONPATH%\python
-import pygame, os, sys
+import os
+import sys
+import pygame
 from pygame import *
 import ezscroll
 
@@ -10,30 +12,38 @@ Gray    = (200,200,200)
 ImageName = 'bgImage.bmp'
 
 def main():
+    """ Sample program for the ezscroll module.
+
+    Tweak! the ez constructor arguments.
+    See! how the event.pos is offset to draw circles.
+    Wonder! at the amout of comments
+    """
     
     pygame.init()
     screen = pygame.display.set_mode(ScrSize)
     world = loadImage(ImageName)
 
-    if not world:
+    if world:
+        pygame.display.set_caption(ImageName)
+    else:        
         pygame.display.set_caption(ImageName + " not found")
         world = pygame.Surface((ScrSize[0]*2, ScrSize[1]*2))
         world.fill(Gray)
  
-
+    # Git it 'n' blit it:
     ez = ezscroll.EZScroll(
-        world,             # what needs to be scrolled
+        world,             # pygame.Surface that needs to be scrolled
         screen.get_rect(), # porthole that limits view
-        20,                # scroll thickness
+        20,                # scroll bar 'thickness'
         ezscroll.BOTH,     # H scrollbars: TOP, BOTTOM, BOTH, or NEITHER
         ezscroll.BOTH,     # V scrollbars: LEFT, RIGHT, BOTH, or NEITHER
         Origin,            # initial position
-        0,                 # internal padding
+        1,                 # internal padding
         True,              # use highlight and drop shadow
         None,              # (trackColor, knobColor)
         None,              # (highlightColor, shadowColor)
         None)              # user-supplied scrollPane
-
+ 
     scrollPane = ez.scrollPane
     screen.blit(scrollPane, Origin)
     pygame.display.flip()
@@ -44,18 +54,24 @@ def main():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
-            
-        # Note: offset event pos by thickness and scrolled distance
-        scrolling, adjPos = ez.handleEvent(event)
-        if event.type == MOUSEBUTTONDOWN and adjPos and not scrolling:   
-           pygame.draw.circle(world, Red, adjPos, 10, 4)  
+
+        # adjPos modifies event.pos by offsets and scrolled distance.        
+        # adjPos is returned as None if the event passed has no pos,
+        # but we'll still limit it to MOUSE events:
+        if event.type in [MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN]:
+
+            scrolling, adjPos = ez.handleEvent(event)
+
+            if event.type == MOUSEBUTTONDOWN and adjPos and not scrolling:   
+               pygame.draw.circle(world, Red, adjPos, 10, 4)  
 
         screen.blit(scrollPane, Origin)
         pygame.display.flip()
 
     
 
-# loads image whether example is run from
+
+# utilty loads image whether example is run from
 # current directory or from import of module.
 def loadImage(ImageName):
 
@@ -63,20 +79,16 @@ def loadImage(ImageName):
     import sys
         
     try: return pygame.image.load(ImageName).convert()
-    except :
-        print "\nUnexpected error:", sys.exc_info()[0]
-        print "probably can't find the image file: ", ImageName
+    except: print ImageName, " not found:", sys.exc_info()[0]
 
     try:
-        print "\nTrying to find it as if running from Python shell..."
         pathHere = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))    
         return pygame.image.load(os.path.join(pathHere,ImageName)).convert()
     except: # name __file__ not recognized -- running from curdir
-        print "Error again!"
+        if pathHere: print "Not found in ", pathHere
 
     finally:
-        print "\nNo luck finding your image file ",\
-              ImageName," in the same dir as ezScroll.py"
+        print ImageName, " not found:", sys.exc_info()[0]
         return None  
     
 
