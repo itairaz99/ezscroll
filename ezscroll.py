cb82b08b5ffedcacd89f2e3801211e61f4748857
hrfeels <hrfeels@c7c4cef6-3a20-11de-be05-5f7a86268029>
2009-05-18 06:29:47 +0000

    drawAtUpdate removed, pretty added but some inconsistent pretty rect drawing.

--- ezscroll.py
+++ ezscroll.py
@@ -6,6 +6,8 @@ from pygame import *
 
 FGCOLOR = 220,220,200
 BGCOLOR = 235,235,230
+HICOLOR = 250,250,245
+LOCOLOR = 40,40,70
 N='N'
 S='S'
 E='E'
@@ -25,19 +27,27 @@ class ScrollPane():
         world,
         pane=None,
         nsew=[S,E],
+        pad=0,
+        pretty=False,
         thick=20,
-        drawAtUpdate=False,        
         fgColor=FGCOLOR,
-        bgColor=BGCOLOR):
+        bgColor=BGCOLOR,
+        hiColor=HICOLOR,
+        loColor=LOCOLOR):
         """ Figures layout and inits ScrollBars """
 
         self.world = world
         self.pane = pane
         self.group = pygame.sprite.RenderUpdates()
         self.nsew = nsew
+        self.pad = pad
         self.thick = thick
-        self.drawAtUpdate = drawAtUpdate
-
+        self.pretty = pretty
+        self.fgColor = fgColor
+        self.bgColor = bgColor
+        self.hiColor = hiColor
+        self.loColor = loColor
+        
         win = self.initViewRect(initRect, self.nsew, self.thick)
         self.viewRect = win
         self.sprites = [] # the scrollbars
@@ -45,15 +55,20 @@ class ScrollPane():
             scrollRect = pygame.Rect(0, win.top, initRect.width, win.height)
             exclude = win.inflate(0, self.thick).move(0,-self.thick//2)
             sb = ScrollBar(self.group, worldSize[1], scrollRect, self.pane, 1,
-                           exclude, fgColor, bgColor)
+                    exclude, self.pad, self.pretty, self.thick,
+                           fgColor, bgColor, hiColor, loColor)
             self.sprites.append(sb)
 
         if N in self.nsew or S in self.nsew:
             scrollRect = pygame.Rect(win.left, 0, win.width, initRect.height)
             exclude = win.inflate(self.thick, 0).move(-self.thick//2,0)
             sb = ScrollBar(self.group, worldSize[0], scrollRect, self.pane, 0,
-                           exclude, fgColor, bgColor)
+                    exclude, self.pad, self.pretty, self.thick,
+                           fgColor, bgColor, hiColor, loColor)
             self.sprites.append(sb)
+
+        self.clearArchive = pygame.Surface(initRect.size).convert()
+        self.clearArchive.fill((255,0,0))
 
     def initViewRect(self, initRect, nsew, thick):
         """ Used by init(), subtract width of scrollbars from viewable area """
@@ -70,15 +85,13 @@ class ScrollPane():
             win.width -= thick               
         return win
 
-    def clear(self, bg, archive):
-        pass # todo
+    def clear(self): #does nothing?
+        self.group.clear(self.pane, self.clearArchive)
         
     def update(self, event):
         """ Called by end user to update scroll state """
         for sb in self.sprites:
-            sb.update(event)
-        if self.drawAtUpdate:
-            self.draw(self.pane)
+            sb.update(event)         
                 
     def draw(self, surface):
         """ Called by end user to draw state to the surface """
@@ -86,13 +99,19 @@ class ScrollPane():
         changes = []
         for sb in self.sprites:
             offsets[sb.axis] = sb.get_offsets()[sb.axis]
-            changes.extend(sb.draw(surface))        
-            # Comment out this blit to see just the scrollbars.    
+            if sb.dirty:
+                changes.extend(sb.draw(surface))
+             
         if changes:
-            changes.append(surface.blit(self.world, self.viewRect.topleft,
-                    (offsets, self.viewRect.size)))
-            pygame.draw.rect(self.pane, BGCOLOR, self.viewRect.inflate(3,3).move(-1,-1), 3)
-            
+            # Comment out this blit to see just the scrollbars.
+            # To date, don't add to changes since the sb includes it.
+            surface.blit(self.world, self.viewRect.topleft,
+                    (offsets, self.viewRect.size))
+        if self.pad and not self.pretty:
+            pygame.draw.rect( #todo better pretty rect here
+                self.pane, self.loColor,
+                self.viewRect.inflate(self.pad+1,self.pad+1), self.pad)
+
         return changes
 
     def get_pane(self):
@@ -113,8 +132,13 @@ class ScrollBar(pygame.sprite.DirtySprite):
         surface=None,
         axis=0,
         exclude=(0,0,0,0),
+        pad=0,
+        pretty=False,
+        thick=20,
         fgColor=FGCOLOR,
-        bgColor=BGCOLOR):
+        bgColor=BGCOLOR,
+        hiColor=HICOLOR,
+        loColor=LOCOLOR):
         
         pygame.sprite.Sprite.__init__(self,group)
         self.initTopleft = initRect.topleft
@@ -125,6 +149,8 @@ class ScrollBar(pygame.sprite.DirtySprite):
         self.axis = axis
         self.fgColor = fgColor
         self.bgColor = bgColor
+        self.hiColor = hiColor
+        self.loColor = loColor        
         self.knob = pygame.Rect(self.rect)
         self.ratio = 1.0* initRect.size[self.axis] / worldDim
         knoblist = list(self.knob.size)
@@ -135,6 +161,9 @@ class ScrollBar(pygame.sprite.DirtySprite):
         self.diff = [0,0]
         self.diff[self.axis] = self.initTopleft[self.axis]
         self.dirty = True
+        self.pad = pad
+        self.pretty = pretty
+        self.thick = thick
 
     def update(self, event): # event must not be None
         """ Called by user with mouse events. event must not be none. """        
@@ -153,15 +182,15 @@ class ScrollBar(pygame.sprite.DirtySprite):
         elif event.type is MOUSEBUTTONUP:
             self.scrolling = False
         
-    def scroll(self, relax):
+    def scroll(self, numPixels):
         """ Moves knob based on mouse events rel change along axis.
         Called internally by update(). Knob travel limited to track.
         """
-        if relax and relax != 0:
+        if numPixels and numPixels != 0:
             axis = self.axis
             rect = self.rect
             knob = self.knob
-            knobMove = max(relax, rect.topleft[axis] - knob.topleft[axis])
+            knobMove = max(numPixels, rect.topleft[axis] - knob.topleft[axis])
             knobMove = min(knobMove, rect.bottomright[axis] - knob.bottomright[axis])
             if knobMove != 0:
                 knobMoves = [0,0]
@@ -175,10 +204,13 @@ class ScrollBar(pygame.sprite.DirtySprite):
         todo: Called by update()>updateViews() if self.auto is True.
         Also mimics group.draw, returning rectangle.
         """
-        if self.dirty == True and surface is not None:
+        if  self.dirty and surface:
             self.dirty = False
             pygame.draw.rect(self.image, self.bgColor, self.rect, 0) 
-            pygame.draw.rect(self.image, self.fgColor, self.knob, 0)            
+            if self.pretty:
+                self.drawPretty()
+            else:
+                pygame.draw.rect(self.image, self.fgColor, self.knob, 0)
             return [surface.blit(self.image, self.initTopleft)]
         else:
             return []
@@ -189,8 +221,49 @@ class ScrollBar(pygame.sprite.DirtySprite):
         """
         return self.leftTop
 
+    def moveRects(self,rects,moves):
+        for rect in rects:
+            rect.move_ip(moves)
+
+    def drawRects(self,rectInfo, surface):
+        for item in rectInfo:
+            pygame.draw.rect(surface, item[0], item[1], item[2])
+
+    def drawPretty(self):
+
+        """ Used internally. Draws drop-shadowed knob if not pretty.
+
+        This drawing method requires the rendering of 6 rects.
+        Three for each knob end, overlapping HiColor, LoColor, FgColor
+        Both are drawn regardless if hidden.
+        Comment out the blit of world here to see exactly.
+
+        """
+        axis = self.axis
+        k = self.knob
+        surface = self.image
+        
+        oppAxis = cmp(0,axis)+1
+        pad = self.pad
+        knob = self.knob
+        size = [knob.width - (pad *2), knob.height - (pad * 2)]
+        size[oppAxis] = self.thick - (2*pad)
+        hiRect = pygame.Rect((knob.left + pad, knob.top + pad), size)
+        loRect = hiRect.inflate(-1,-1).move(1,1)
+        fgRect = loRect.inflate(-1,-1)
+        rectInfo = ((self.hiColor, hiRect, 1),
+                    (self.loColor, loRect, 1),
+                    (self.fgColor, fgRect, 0))
+        
+        self.drawRects(rectInfo, surface)            
+        moves = [0,0]
+        moves[oppAxis] = knob.size[oppAxis] - size[oppAxis] - (2*pad)
+        self.moveRects((hiRect, loRect, fgRect), moves)
+        self.drawRects(rectInfo,surface)
 
 
+        
 if __name__ == '__main__':
     import examples
     examples.examples()
+
