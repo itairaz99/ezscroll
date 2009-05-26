3878fc65a889ab1ab9603cdf5034cb53b7be1b4b
hrfeels <hrfeels@c7c4cef6-3a20-11de-be05-5f7a86268029>
2009-05-26 22:06:03 +0000

    added third example, vertical scrollbar which has scroll(int) called as would be called by external code.

--- examples.py
+++ examples.py
@@ -14,7 +14,7 @@ def examples():
     puts the scrollbars in a sprite group, and blits the world.
     If you just want one scrollbar, still may be easier to
     use ScrollPane and pass [S] or [E], etc.
-    Closing window of first example proceeds to next.
+    Closing window of proceeds through examples.
     """    
     pygame.init()
     screen = pygame.display.set_mode(ScrSize)
@@ -30,8 +30,15 @@ def examples():
     bg.fill(BGCOLOR)
     pygame.display.set_caption("Example 1:  ScrollPane")
     initRect = pygame.Rect(screen.get_rect())
-    sp = ScrollPane(world.get_size(), initRect, world, bg,
-                    [S, W, N], 3, True, 20)
+    sp = ScrollPane(
+        world.get_size(),
+        initRect,
+        world,
+        bg,
+        [S, W, N],
+        3,
+        True,
+        20)
     sp.draw(bg)
     screen.blit(bg,Origin)
     pygame.display.flip()
@@ -49,11 +56,22 @@ def examples():
     pygame.display.set_caption("Example 2:    ScrollBar")
     thick = 20
     scrollRect = pygame.Rect(0, 0, ScrSize[0], thick)
-    excludes = ((0, thick), ScrSize)
+    excludes = ((0, thick), ScrSize) # rect where sb update is a pass
     group = pygame.sprite.RenderPlain()    
-    sb = ScrollBar(group, world.get_width(), scrollRect, bg,
-                   0, excludes, 4, True, thick,
-                   (170,220,180), (200,210,225), (240,240,250), (0,55,100))    
+    sb = ScrollBar(
+        group,
+        world.get_width(),
+        scrollRect,
+        bg,
+        0,
+        excludes,
+        4,
+        True,
+        thick,
+        (170,220,180),
+        (200,210,225),
+        (240,240,250),
+        (0,55,100))    
     sb.draw(bg)
     bg.blit(world, (0,thick),
             (sb.get_scrolled(),(ScrSize[0],ScrSize[1]-thick)))   
@@ -61,18 +79,70 @@ def examples():
     pygame.display.flip()
     while True:
         event = pygame.event.wait()
-        if event.type is pygame.QUIT: break        
+        if event.type is pygame.QUIT: break
         sb.update(event)
         changes = sb.draw(bg)
         if len(changes) > 0:
             changes.append(bg.blit(world, (0,thick),
-                          (sb.get_scrolled(),(ScrSize[0],ScrSize[1]-thick))))
+                  (sb.get_scrolled(),(ScrSize[0],ScrSize[1]-thick))))
         screen.blit(bg,Origin)
         pygame.display.update(changes)
+        
+##        # you can skip even sending any events in the view area       
+##        try: 
+##            if not sb.exclude.collidepoint(event.pos):
+##                sb.update(event)
+##                ...
+
+    ###  EXAMPLE 3
+    import random
+    pygame.display.set_caption("Example 3: ScrollBar")
+    thick = 20
+    scrollRect = pygame.Rect(0, 0, thick,ScrSize[1])
+    excludes = ((thick, 0), ScrSize) # rect where sb update is a pass
+    group = pygame.sprite.RenderPlain()
+    sb = ScrollBar(
+        group,
+        world.get_height(),
+        scrollRect,
+        bg,
+        1,
+        excludes,
+        4,
+        False,
+        thick,
+        (120,120,160,10),
+        (88,57,99,214))
+    sb.draw(bg)
+    bg.blit(world, (thick,0),(sb.get_scrolled(),
+                              (ScrSize[0]-thick,ScrSize[1])))   
+    screen.blit(bg, Origin)
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+    counter = 0
+    while True:
+        counter += clock.tick()
+        if counter > 1000: chichi = 0
+        if counter % 100 == 0:
+            scrollAmount = random.randrange(-20, 20, 1) 
+            sb.scroll(scrollAmount)
+        changes = sb.draw(bg)
+        if len(changes) > 0:
+            scrolled = sb.get_scrolled()
+            changes.append(bg.blit(
+                world,
+                (thick,0),
+                (scrolled[0]+thick,
+                scrolled[1],
+                ScrSize[0],ScrSize[1])))
+        screen.blit(bg,Origin)
+        pygame.display.update(changes)
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
 
 
-    pygame.quit()
-    sys.exit(0)
 
 if __name__ == '__main__':
     examples()
